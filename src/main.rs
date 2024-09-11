28af7094b639a0a364733c8ea8862514c86ee7c2
matt <matt.barson@gmail.com>
2024-09-11 21:59:59 +0100

    Add api struts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+mod structs;
+
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
+use handlebars::Handlebars;
+
+
+
+/// Pull the data from the NASA api.
+async fn get_nasa_api(req: web::Json<structs::Root>) -> Result<String> {
+    Ok(format!("{:?}", req.links))
+
+}
+
+
+/// Function to call and serve handlebars enabled html index page
+///
+#[get("/")]
+async fn index(hb: web::Data<Handlebars<'_>>) -> impl Responder {
+    todo!()
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
